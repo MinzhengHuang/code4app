3593a9d97067651ad1bf5cfc2dbeca53b34005f5
黄敏争 <ahtchmz@163.com>
2015-12-07 20:42:59 +0800

    新建项目

---
+++ code4app/src/com/code4app/Code4appApplication.java
@@ -0,0 +1,16 @@
+package com.code4app;
+
+import android.app.Application;
+
+/**
+ * Created by huangminzheng on 15/11/26.
+ */
+public class Code4appApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
